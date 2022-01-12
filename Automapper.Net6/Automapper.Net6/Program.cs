38c3f3668d9a5c484d7aff908695473ea2a36af9
Konstantin Peshekhonov <konstantin.peshekhonov@cloudmore.com>
2022-01-12 12:25:32 +0200

    some comments added

--- Automapper.Net6/Automapper.Net6/Program.cs
+++ Automapper.Net6/Automapper.Net6/Program.cs
@@ -45,29 +45,38 @@ record TestModelB(string Id, DateTimeOffset EventTime)
     }
 }
 
+/// <summary>
+/// Fails validation due to first two properties being mapped using Expression
+/// </summary>
 class AutomapperProfileA : Profile
 {
     public AutomapperProfileA() 
         => CreateMap<StreamEntry, TestModelB>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.EventTime, opt => opt.MapFrom(src => DateTimeOffset.Parse(src["EventTime"])))
-            .ForAllMembers(opt => opt.MapFrom(src => src[opt.DestinationMember.Name]));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id)) // Using Expression<Func>
+            .ForMember(dest => dest.EventTime, opt => opt.MapFrom(src => DateTimeOffset.Parse(src["EventTime"]))) // Using Expression<Func>
+            .ForAllMembers(opt => opt.MapFrom(src => src[opt.DestinationMember.Name])); // Using Expression<Func>
 }
 
+/// <summary>
+/// Succeeds validation due to first two properties being mapped using Func and generic mapping done using Expression
+/// </summary>
 class AutomapperProfileB : Profile
 {
     public AutomapperProfileB()
         => CreateMap<StreamEntry, TestModelB>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom((src, _) => src.Id))
-            .ForMember(dest => dest.EventTime, opt => opt.MapFrom((src, _) => DateTimeOffset.Parse(src["EventTime"])))
-            .ForAllMembers(opt => opt.MapFrom(src => src[opt.DestinationMember.Name]));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom((src, _) => src.Id)) // Using Func instead of Expression<Func>
+            .ForMember(dest => dest.EventTime, opt => opt.MapFrom((src, _) => DateTimeOffset.Parse(src["EventTime"]))) // Using Func instead of Expression<Func>
+            .ForAllMembers(opt => opt.MapFrom(src => src[opt.DestinationMember.Name])); // Still using Expression<Func>
 }
 
+/// <summary>
+/// Fails validation due to generic mapping being done using Func, not expression
+/// </summary>
 class AutomapperProfileC : Profile
 {
     public AutomapperProfileC()
         => CreateMap<StreamEntry, TestModelB>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom((src, _) => src.Id))
-            .ForMember(dest => dest.EventTime, opt => opt.MapFrom((src, _) => DateTimeOffset.Parse(src["EventTime"])))
-            .ForAllMembers(opt => opt.MapFrom((src, _) => src[opt.DestinationMember.Name]));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom((src, _) => src.Id)) // Using Func instead of Expression<Func>
+            .ForMember(dest => dest.EventTime, opt => opt.MapFrom((src, _) => DateTimeOffset.Parse(src["EventTime"]))) // Using Func instead of Expression<Func>
+            .ForAllMembers(opt => opt.MapFrom((src, _) => src[opt.DestinationMember.Name])); // Using Func instead of Expression<Func>
 }
